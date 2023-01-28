09c1a389f4925b52e21feb7bebf23a9f3f4c854a
sharktank-tech <danilokog652@gmail.com>
2023-01-28 16:40:10 -0300

    Atualizações de nome, add da tela inicial

--- Tela_de_menu/main.py
+++ Tela_de_menu/main.py
@@ -2,30 +2,21 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.uix.label import Label
-from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.lang import Builder
-from kivy.uix.textinput import TextInput
 
+Builder.load_file("layout.kv")
 
-
-Builder.load_file('layout.kv')
-
-
-class TelaLogin_layout(Widget):
+class TelaMenu_layout(Widget):
     def build(self):
         return Button
-        return Image
         return Label
-        return TextInput
-
-
 
 class Banco_Arfly(App):
     def build(self):
-        Window.clearcolor = (1,1,1,1)
-        return TelaLogin_layout()
+        Window.clearcolor = (0,0,205)
+        return TelaMenu_layout()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Banco_Arfly().run()
